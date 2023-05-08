93ba8decdf1e6cb2eb8fc6391cf3a58c899d4d48
Savannah <skruse101@gmail.com>
2023-05-08 13:02:28 -0700

    javascript events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor ="purple";
+dodger.style.bottom = "100px";
+function moveDodgerRight(){
+    const number = dodger.style.left.replace('px', '');
+    const left = parseInt(number, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left + 1}px`; 
+    }
+}
+function moveDodgerLeft(){
+    const number = dodger.style.left.replace("px", "")
+    const left = parseInt(number, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (e.key ==="ArrowRight") {
+        moveDodgerRight();
+    }
+});
